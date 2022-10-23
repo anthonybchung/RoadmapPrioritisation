4ac3511641a8d832249c337b70a6921950270be1
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-10-23 19:09:35 +1100

    users MVC files added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
 const colors = require('colors');
 const { app, PORT, HOST, MODE } = require('./server');
 
+colors.setTheme({
+  modeStyle: ['green', 'inverse', 'bold'],
+});
 const server = app.listen(PORT, HOST, () => {
   console.log('ExpressJS server in:');
-  console.log(colors.yellow('MODE:', MODE));
+  if (MODE == 'development') console.log('MODE:' + 'development'.modeStyle);
+  else console.log('MODE:' + 'production'.modeStyle);
   console.log('HOST: %s', HOST);
   console.log('PORT: %s', PORT);
 });

cf45f21aaee53963a87d29423ce2c3c54545d16c
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-10-24 01:13:50 +1100

    UserController updateUser
    updateUser can update data on Mongo Atlas

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -65,13 +65,28 @@ exports.createUser = async (req, res, next) => {
 // Description: Update users
 // route: PUT /api/v1/users/:id
 // access: private
-exports.updateUser = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: {
-      msg: `user id: ${req.params.id} updated`,
-    },
-  });
+exports.updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // Description: Delete user

5da7fd9f671db9264ce43907844fababbb33b9d7
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-10-25 09:17:04 +1100

    Auth Encrypt password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,8 +2,20 @@ const User = require('../models/User.model');
 const ErrorResponse = require('../utils/errorResponse');
 
 // Description: Register User.
-// route: GET /api/v1/auth/register
+// route: POST /api/v1/auth/register
 // access: public
 exports.register = async (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'register user' });
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };

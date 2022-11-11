43c3f71405a8fe0ccc9259a1c7888ff54a99f015
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-11 23:12:23 +1100

    seeder can insert data from a json file and delete all data in a collection

--- src/seeder.js
+++ src/seeder.js
@@ -7,19 +7,12 @@ dotenv.config({ path: "../.env" });
 //connect to database
 mongoose.connect(process.env.MONGO_URI);
 
-// Load model
-const User = require("./models/User.model");
-
-//Data file
-const users = JSON.parse(
-  fs.readFileSync("./testing/seeder/users.json", "utf-8")
-);
-
 //insert into database
-const insertData = async () => {
+const insertData = async (Model, data) => {
   try {
-    await User.create(users);
+    await Model.create(data);
     console.log("data has been inserted");
+    mongoose.connection.close();
     process.exit;
   } catch (err) {
     console.log(err);
@@ -27,12 +20,36 @@ const insertData = async () => {
 };
 
 //delete all data in database
-const deleteData = async () => {
+const deleteData = async (Model) => {
   try {
-    await User.deleteMany();
+    await Model.deleteMany();
     console.log("all data deleted.");
+    mongoose.connection.close();
     process.exit;
   } catch (err) {
     console.log(err);
   }
 };
+
+// description:
+// insert data:   node seeder -i Users
+// delete data:   node seeder -d Users
+//                node seeder 2   3
+const command = process.argv[2];
+const table = process.argv[3];
+
+//Data file
+const tableLower = table.toLocaleLowerCase();
+const data = JSON.parse(
+  fs.readFileSync(`./testing/seeder/${tableLower}.json`, "utf-8")
+);
+
+// Load model
+const modelName = table.charAt(0).toUpperCase() + table.slice(1);
+const Model = require(`./models/${modelName}.model`);
+
+if (process.argv[2] === "-i") {
+  insertData(Model, data);
+} else if (process.argv[2]) {
+  deleteData(Model);
+}

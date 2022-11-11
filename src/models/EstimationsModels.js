696c2a321aaf7d43b47a2f1c5c195ec35dcd55dd
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-11 21:03:00 +1100

    estimation schema typo

--- src/models/EstimationsModels.js
+++ src/models/EstimationsModels.js
@@ -1,92 +1,92 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const EstimationsSchema = new mongoose.Schema({
-      priority: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter Priority'],
-        maxlength: [2, "Priority max length is 2" ],
-        default: 0,
-      },
-      ticket_id: {
-        type: String,
-        unique: true,
-        required: [true, 'Please enter a ticket_id'],
-        maxlength: [7, "Ticket_id max length is 7" ],
-        trim: true,
-      },
-      initiative: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter a initiative title'],
-        maxlength: [30, "Title max length is 30" ],
-        trim: true,
-      },
-      description: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter description'],
-        maxlength: [200, "Decription max length is 200" ],
-        trim: true,
-      },
-      submit_data: {
-        type: Date,
-        unique: false,
-        required: [true, 'Please enter sumbit date'],
-        default: Date.now,
-      },
-      owner: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter owner name'],
-        maxlength: [20, "Owner max length is 20" ],
-        default: 0,
-      },
-      squad_name: {
-        type: String,
-        unique: true,
-        required: [true, 'Please enter a squad name'],
-        maxlength: [20, "Squad_name max length is 20" ],
-        default: 0,
-      },
-      goal: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter goal details'],
-        maxlength: [150, "Owner max length is 150" ],
-        default: 0,
-      },
-      purpose: {
-        type: String,
-        unique: false,
-        required: [true, 'Please enter goal details'],
-        maxlength: [200, "Owner max length is 200" ],
-        default: 0,
-      },
-      eng_est: {
-        type: Number,
-        unique: false,
-        required: [true, 'Please enter Engineer estimation'],
-        default: 0,
-      },
-      design_est: {
-        type: Number,
-        unique: false,
-        required: [true, 'Please enter Design estimation'],
-        default: 0,
-      },
-      pm_est: {
-        type: Number,
-        unique: false,
-        required: [true, 'Please enter PM estimation'],
-        default: 0,
-      },
-      comment: {
-        type: String,
-        unique: false,
-        maxlength: [200, "Owner max length is 200" ],
-        default: 0,
-      },
-});    
+  priority: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter Priority"],
+    maxlength: [2, "Priority max length is 2"],
+    default: 0,
+  },
+  ticket_id: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter a ticket_id"],
+    maxlength: [7, "Ticket_id max length is 7"],
+    trim: true,
+  },
+  initiative: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter a initiative title"],
+    maxlength: [30, "Title max length is 30"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter description"],
+    maxlength: [200, "Decription max length is 200"],
+    trim: true,
+  },
+  submit_data: {
+    type: Date,
+    unique: false,
+    required: [true, "Please enter sumbit date"],
+    default: Date.now,
+  },
+  owner: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter owner name"],
+    maxlength: [20, "Owner max length is 20"],
+    default: 0,
+  },
+  squad_name: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter a squad name"],
+    maxlength: [20, "Squad_name max length is 20"],
+    default: 0,
+  },
+  goal: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter goal details"],
+    maxlength: [150, "Owner max length is 150"],
+    default: 0,
+  },
+  purpose: {
+    type: String,
+    unique: false,
+    required: [true, "Please enter goal details"],
+    maxlength: [200, "Owner max length is 200"],
+    default: 0,
+  },
+  eng_est: {
+    type: Number,
+    unique: false,
+    required: [true, "Please enter Engineer estimation"],
+    default: 0,
+  },
+  design_est: {
+    type: Number,
+    unique: false,
+    required: [true, "Please enter Design estimation"],
+    default: 0,
+  },
+  pm_est: {
+    type: Number,
+    unique: false,
+    required: [true, "Please enter PM estimation"],
+    default: 0,
+  },
+  comment: {
+    type: String,
+    unique: false,
+    maxlength: [200, "Owner max length is 200"],
+    default: 0,
+  },
+});
 
-module.exports = mongoose.model('Estimations', EstimationsSchema);    
+module.exports = mongoose.model("Estimation", EstimationsSchema);
